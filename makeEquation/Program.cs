f49c382847fd4f5b008e93f0d9748fd3f81952cc
Steven Tharwat <stevven.helmy@gmail.com>
2025-05-22 20:07:58 +0300

    V1.0

---
+++ makeEquation/Program.cs
@@ -0,0 +1,22 @@
+﻿
+for (int i = 0 ; i <= 40 ; i++)
+{
+    string eq = generateEq(i);
+    Console.WriteLine($"{eq} = {i} \n");
+}
+
+string generateEq(double number)
+{
+    while (true)
+    {
+        Random r1 = new();
+        int n1 = r1.Next(1, 100);
+        int n2 = r1.Next(1, 100);
+        int n3 = r1.Next(1, 100);
+        int n4 = r1.Next(1, 100);
+        if (( n1 * n2 ) - ( n4 + n3 ) == number)
+        {
+            return $"({n1} x {n2}) - ({n4} + {n3})";
+        }
+    }
+}
